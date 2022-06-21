95f84aea2027b387fde74a068fa022dedb543ea4
Shahid Hussain <sh604780@gmail.com>
2022-06-21 21:01:10 +0530

    Delete shivangi.py

--- shivangi.py
+++
@@ -1,9 +0,0 @@
-x=5
-y=10
-#x=input('enter the value of x:')
-#y=input('enter the value of y:')
-temp=x
-x=y
-y=temp
-print('the value of x after swapping:{}'.format(x))
-print('the value of y after swapping:{}'.format(y))
